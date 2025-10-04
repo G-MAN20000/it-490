413f3f60f431e90a425d7b4fc5c65f604fe42a96
Jerry Suarez (JS2624) <bobsuarez789@gmail.com>
2025-10-03 21:30:01 -0400

    Add files via upload

---
+++ njitfall2025/src/game/scenes/LevelSelectionScene.ts
@@ -0,0 +1,46 @@
+import { TexturedButton } from '../objects/ui/TexturedButton.ts';
+import { TextStyles } from '../objects/ui/TextStyles.ts';
+import Phaser from 'phaser';
+
+export class LevelSelectionScene extends Phaser.Scene {
+    private backgroundMusic?: Phaser.Sound.BaseSound;
+
+    constructor() {
+        super('LevelSelection');
+    }
+
+    preload() {
+        if (!this.cache.audio.exists('menu_music')) {
+            this.load.audio('menu_music', new URL('../assets/music/Firstlevel.mp3', import.meta.url).href);
+        }
+    }
+
+    create() {
+        this.backgroundMusic = this.sound.add('menu_music', { loop: true, volume: 0.5 });
+        this.backgroundMusic.play();
+
+        this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+            this.backgroundMusic?.stop();
+            this.backgroundMusic?.destroy();
+            this.backgroundMusic = undefined;
+        });
+
+        const mainMenuButton = new TexturedButton(this, this.scale.width / 2, this.scale.height / 2, 120, 40, 'Main Menu', null, TextStyles.BUTTON_TEXT);
+
+        mainMenuButton.backGround.on('pointerdown', () => {
+            this.scene.start('MainMenu');
+            console.log('Switched to main menu');
+        });
+
+        const levelOneButton = new TexturedButton(this, this.scale.width / 2, this.scale.height / 2 + 41, 120, 40, 'Start Level 1', null, TextStyles.BUTTON_TEXT);
+
+        levelOneButton.backGround.on('pointerdown', () => {
+            this.scene.start('LevelOne');
+            console.log('Switched to level One');
+        });
+
+        this.add.existing(mainMenuButton);
+        this.add.existing(levelOneButton);
+    }
+
+}
